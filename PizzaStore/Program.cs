95a5a37f487ccede4d3c55e5ef413b308656a3cd
Tạ Long Khánh <79636784+khanhtl@users.noreply.github.com>
2023-10-09 10:22:36 +0000

    feat: crud with inmemory database

--- PizzaStore/Program.cs
+++ PizzaStore/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.OpenApi.Models;
+using PizzaStore.Db;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
@@ -17,5 +18,10 @@ app.UseSwaggerUI(c => {
 });
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/pizzas/{id}", (int id) => PizzaDb.GetPizza(id));
+app.MapGet("/pizzas", () => PizzaDb.GetPizzas());
+app.MapPost("/pizzas", (Pizza pizza) => PizzaDb.CreatePizza(pizza));
+app.MapPut("/pizzas", (Pizza pizza) => PizzaDb.UpdatePizza(pizza));
+app.MapDelete("/pizzas/{id}", (int id) => PizzaDb.RemovePizza(id));
 
 app.Run();
